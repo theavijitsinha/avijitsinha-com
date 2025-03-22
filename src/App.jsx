6a2973b187a843964f4d565ccedc2639818a4618
Avijit Sinha <theavijitsinha@gmail.com>
2025-03-22 12:36:50 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import constructionGif from '/under-construction.gif'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={constructionGif} className="logo" alt="Under construction" />
+      </div>
+      <h1>Under construction</h1>
+    </>
+  )
+}
+
+export default App
